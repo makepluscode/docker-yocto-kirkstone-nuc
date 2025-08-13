7f43f55fc943ca8bbf5fb33f6c148c7ed61f852b
makepluscode <makepluscode@gmail.com>
2025-08-13 20:22:43 +0900

    refactor: Remove deprecated meta-rauc-cmake layer

--- kirkstone/meta-rauc-cmake/recipes-core/rauc/rauc-cmake-1.13/include/service.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <glib.h>
-
-gboolean r_service_run(void)
-G_GNUC_WARN_UNUSED_RESULT;
-void set_last_error(const gchar *message);
--- kirkstone/meta-rauc-cmake/recipes-core/rauc/rauc_1.13.bb
+++
@@ -1,3 +0,0 @@
-require rauc.inc
-require rauc-target.inc
-require rauc-1.13.inc
